0ac636f3de075e4b17870fe6c987cc754d1f749c
Rohan Hitchcock <rhitchcock@student.unimelb.edu.au>
2024-07-25 17:54:39 +1000

    work from live session

--- workshop3/mlp.py
+++ workshop3/mlp.py
@@ -33,7 +33,7 @@ Challenge:
 
 * manually register the MLP modules as pytrees (obviating equinox dependency)
 """
-
+import math
 from typing import Literal
 from jaxtyping import Array, Float, Int, PRNGKeyArray as Key
 
@@ -49,8 +49,52 @@ import mattplotlib as mp
 # # # 
 # Architecture
 
+class LinearLayer(equinox.Module):
 
-# TODO
+    weight_matrix: Array # from jaxtyping
+    bias_vector: Array
+
+    def __init__(self, key: Key, num_inputs: int, num_outputs: int):
+        
+        # Xavier init
+        init_bound = jnp.sqrt(6 / (num_inputs * num_outputs))
+
+        self.weight_matrix = jax.random.uniform(
+            key, 
+            shape=(num_inputs, num_outputs), 
+            minval=-init_bound, 
+            maxval=init_bound
+        )
+
+        self.bias_vector = jnp.zeros(num_outputs)
+
+
+    def __call__(self, x: Float[Array, '... num_inputs']) -> Float[Array, '... num_outputs']:
+        return x @ self.weight_matrix + self.bias_vector
+
+class MLPImageClassifier(equinox.Module):
+    layer1: LinearLayer
+    layer2: LinearLayer
+
+    def __init__(self, key: Key, image_shape: tuple[int, int], num_hidden: int, num_classes: int):
+        key_layer1, key_layer2 = jax.random.split(key)
+        num_inputs = math.prod(image_shape)
+        self.layer1 = LinearLayer(key_layer1, num_inputs, num_hidden)
+        self.layer2 = LinearLayer(key_layer2, num_hidden, num_classes)
+
+
+    def __call__(self, x: Float[Array, '... image_height image_width']) -> Float[Array, '... num_classes']:
+
+        x = einops.rearrange(x, '... h w -> ... (h w)')
+
+        x = self.layer1(x)
+        x = jnp.tanh(x) # or jax.nn.relu
+        x = self.layer2(x)
+
+        x = jax.nn.softmax(x, axis=-1)
+
+        return x
+        
 
 
 # # # 
@@ -67,29 +111,54 @@ def main(
     seed: int = 42,
 ):
     key = jax.random.key(seed)
+    key_init_model, key = jax.random.split(key)
 
 
+    
     # initialise model
     print("initialising model...")
-    print("TODO.")
-
+    model = MLPImageClassifier(key_init_model, image_shape=(28, 28), num_hidden=num_hidden, num_classes=10)
+    print(model)
+    print(model(jnp.zeros((2, 28, 28))))
 
     print("loading and preprocessing data...")
-    print("TODO.")
+    with jnp.load('mnist.npz') as datafile:
+        x_train = datafile['x_train']
+        x_test = datafile['x_test']
+        y_train = datafile['y_train']
+        y_test = datafile['y_test']
+
+    x_train, x_test, y_train, y_test = jax.tree.map(jnp.array, (x_train, x_test, y_train, y_test))
+    x_train, x_test = jax.tree.map(lambda x : x / 255, (x_train, x_test))
+
+    print(vis_digits(
+        digits=x_test[:num_digits_per_visualisation], 
+        true_labels=y_test[:num_digits_per_visualisation], 
+        # pred_labels=model(x_test[:num_digits_per_visualisation]),
+    ))
+
 
 
     print("begin training...")
     losses = []
     accuracies = []
     for step in tqdm.trange(num_steps, dynamic_ncols=True):
+        
         # sample a batch
-        tqdm.tqdm.write("TODO: sample a batch...")
+        key_batch, key = jax.random.split(key)
+        batch_indices = jax.random.choice(key=key_batch, a=y_train.size, shape=(batch_size,), replace=False)
+        x_batch = x_train[batch_indices]
+        y_batch = y_train[batch_indices]
 
         # compute the batch loss and grad
-        tqdm.tqdm.write("TODO: compute batch loss and grad...")
+        loss, grads = jax.value_and_grad(cross_entropy)(
+            model, 
+            x_batch, y_batch
+        )
 
+        
         # update model
-        tqdm.tqdm.write("TODO: update model (and optimiser)...")
+        model = jax.tree.map(lambda w, g: w - learning_rate * g, model, grads)
 
         break
 
@@ -122,6 +191,7 @@ def main(
 
 # # # 
 # Metrics
+
 
 
 # TODO
