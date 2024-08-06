fixed numerical stability issues in dlns

projects/dln/__main__.py
@@ -6,8 +6,11 @@ import jax.numpy as jnp
 
 from dln.model import DeepLinearNetwork
 from dln import llc
-from dln.train import generate_dataset
+from dln.train import generate_dataset, prepare_data, generate_data
 from shared.samplers_optax import SGLD
+
+import matplotlib.pyplot as plt
+
 
 import optax
 
@@ -92,8 +95,9 @@ if __name__ == "__main__":
     parser.add_argument("--num_experiments", type=int, default=100)
     parser.add_argument("--gamma", type=float, default=1.0)
     parser.add_argument("--beta", type=float, default=1.0)
+    parser.add_argument("--epsilon", type=float, default=None)
     parser.add_argument("--seed", type=int, default=0)
-    parser.add_argument("--batch_size", type=int, default=512)
+    parser.add_argument("--batch_size", type=int, default=500)
     parser.add_argument("--num_chains", type=int, default=1)
     parser.add_argument("--burnin_prop", type=float, default=0.9)
 
@@ -103,55 +107,78 @@ if __name__ == "__main__":
     key = jax.random.key(args.seed)
 
     num_layers_min, num_layers_max, min_width, max_width, epsilon, num_steps, n = get_params(args.oom)
+
+    if args.epsilon is not None:
+        epsilon = args.epsilon
+
     nbeta = llc.nbeta(args.beta, n)
-    # nbeta = llc.nbeta(args.beta, args.batch_size)
+    
     sampler = SGLD(epsilon, args.gamma, nbeta, args.seed)
 
     sample_llc_multichain = jax.jit(jax.vmap(llc.sample_llc, in_axes=(None, None, 0)), static_argnames=['sampler'])
-    # sample_llc_multichain = jax.vmap(llc.sample_llc, in_axes=(None, None, 0))
     
+    loss_fn = lambda m, x, y : jnp.mean((m(x) - y) ** 2)
+    val_grad_loss_fn = jax.value_and_grad(loss_fn)
 
-    
-
-    for _ in tqdm.trange(args.num_experiments):
+    for eid in tqdm.trange(args.num_experiments):
         
         key, key_model = jax.random.split(key)
         init_model = DeepLinearNetwork.initialize_true(key_model, num_layers_min, num_layers_max, min_width, max_width)
 
+        # print(f"{init_model.dim_in=}")
+        # print(f"{init_model.dim_out=}")
+
+
         key, key_data = jax.random.split(key)
-        data = generate_dataset(key_data, args.num_chains * num_steps, args.batch_size, init_model.layer_sizes[0])
-        data = data.reshape((args.num_chains, num_steps, args.batch_size, -1))
+        data = generate_data(key_data, n, init_model.dim_in)
 
 
-        print(data.shape)
-        print(init_model(data[0][0][0]))
+        # print(f"{data.shape=}")
+        outputs = init_model(data)
+
+        # print(f"{outputs.shape=}")
+
+        
 
 
-        loss_fn = lambda updated_model, x : ((init_model(x) - updated_model(x)) ** 2).mean()
-        val_grad_loss_fn = jax.value_and_grad(loss_fn)
+        key, key_dataset = jax.random.split(key)
+        inputs, targets = prepare_data(key_dataset, data, outputs, num_steps, args.batch_size)
+        # print(f"{dataset.shape=}")
 
-        optimizer_state = sampler.init(init_model)
+
+        l = jax.vmap(loss_fn, in_axes=(None, 0, 0))(init_model, inputs, targets).mean()
+        # print("Mean loss ", l)
+
+    
+        # loss_fn = lambda updated_model, x : ((init_model(x) - updated_model(x)) ** 2).mean()
+        
+
+        key, key_sampler = jax.random.split(key)
+
+        optimizer_state = sampler.init(init_model, key_sampler)
         model = init_model
-        for i, x in enumerate(data[0]):
+        trace = []
+        for i, (x, y) in enumerate(zip(inputs, targets)):
 
-            loss, grad_loss = val_grad_loss_fn(model, x)
+            loss, grad_loss = val_grad_loss_fn(model, x, y)
             updates, optimizer_state = sampler.update(grad_loss, optimizer_state, model)
-            model = optax.apply_updates(model, updates)
+            model = jax.tree.map(lambda p, u : p + u, model, updates)
             
+            trace.append(loss)
+            if jnp.isnan(loss):
+                break
             # model = jax.tree.map(lambda w, g: w - 0.00435 * g, model, grad_loss)
             
 
-            print(f"min_grad: {min_tree(grad_loss)}, max_grad={max_tree(grad_loss)}")
+            """print(f"min_grad: {min_tree(grad_loss)}, max_grad={max_tree(grad_loss)}")
             print(f"min_update: {min_tree(updates)}, max_update={max_tree(updates)}")
             print(f"{loss.item()=}")
 
             if jnp.isnan(loss) or i > 30:
-                exit()
+                exit()"""
 
 
-        
-        model = init_model
-
+        """
         traces = sample_llc_multichain(model, sampler, data)
         estimated_llc_per_chain = jax.vmap(llc.estimate_llc, in_axes=(0, None, None))(traces, int(args.burnin_prop * num_steps), nbeta)
 
@@ -159,14 +186,25 @@ if __name__ == "__main__":
         estimated_llc = jnp.mean(estimated_llc_per_chain, where=nan_mask)
 
 
+        
         traces = llc.sample_llc(model, sampler, data[0])
-        estimated_llc = llc.estimate_llc(traces, int(args.burnin_prop * num_steps), nbeta)
+        
+        """
 
-        theoretical_llc = model.theoretical_llc()
+        trace = jnp.array(trace)
+        estimated_llc = llc.estimate_llc(trace, int(args.burnin_prop * num_steps), nbeta)
+
+        moving_llc_estimate = llc.llc_moving_mean(trace[0], trace, nbeta)
+        fig, ax = plt.subplots()
+        ax.plot(trace)
+        plt.savefig(f"debug/exp={eid}.png")
+        plt.close(fig)
+
+        theoretical_llc = init_model.theoretical_llc()
 
 
-        print(model.num_parameters, model.rank(), theoretical_llc, estimated_llc, jnp.count_nonzero(nan_mask))
-        print(traces[:30])
+        print(f"Exp {eid}: ", model.num_parameters, init_model.rank(), theoretical_llc, estimated_llc)
+        
 
         
 

projects/dln/llc.py
@@ -2,11 +2,11 @@ import jax
 import jax.numpy as jnp
 import optax
 from optax import GradientTransformation
-from jaxtyping import Array
+from jaxtyping import Array, Key
 from functools import partial
 
 
-def sample_llc(model, sampler: GradientTransformation, data: Array):
+def sample_llc(key: Key, model, sampler, data: Array):
 
     init_model = model
 
@@ -18,12 +18,12 @@ def sample_llc(model, sampler: GradientTransformation, data: Array):
         model, optimizer_state = carry
         loss, grad_loss = val_grad_loss_fn(model, x)
         updates, optimizer_state = sampler.update(grad_loss, optimizer_state, model)
-        model = optax.apply_updates(model, updates)
+        model = jax.tree.map(lambda p, u : p + u, model, updates)
 
         return (model, optimizer_state), loss
 
 
-    opt_state = sampler.init(model)
+    opt_state = sampler.init(model, key)
     _, trace = jax.lax.scan(step, (init_model, opt_state), data)
 
     return trace
@@ -34,7 +34,7 @@ def llc_mean(init_loss, loss_trace, nbeta):
 
 def llc_moving_mean(init_loss, loss_trace, beta):
     moving_mean_loss = jnp.cumsum(loss_trace) / (jnp.arange(len(loss_trace)) + 1)
-    return (moving_mean_loss - init_loss) / beta
+    return (moving_mean_loss - init_loss) * beta
 
 
 def nbeta(beta, n):

projects/dln/model.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 import itertools
 
 import jax
@@ -9,6 +9,15 @@ from dln.dln_llc_constraint_problem import compute_delta_sigma
 from shared.utils import register_model
 
 import math
+
+
+def xavier_uniform_init(key: Key, shape: Tuple[int, int]) -> Float[Array, "shape[0] shape[1]"]:
+    bound = jnp.sqrt(6 / (shape[0] + shape[1]))
+    return jax.random.uniform(key, shape, minval=-bound, maxval=bound)
+
+def xavier_normal_init(key: Key, shape: Tuple[int, int]) -> Float[Array, "shape[0] shape[1]"]:
+    std = jnp.sqrt(2 / (shape[0] + shape[1]))
+    return std * jax.random.normal(key, shape)
 
 @register_model(weights=['layers'])
 class DeepLinearNetwork:
@@ -30,7 +39,12 @@ class DeepLinearNetwork:
         # x has shape [B, N] where N is the input dimension and B is the batch 
         # size, so our vectors are row vectors. jnp.linalg.multi_dot figures out 
         # the optimal order to several successive matrix multiplications
-        return jnp.linalg.multi_dot([x, *self.layers])
+        # return jnp.linalg.multi_dot([x, *self.layers])
+        result = x
+        for layer in self.layers:
+            result = result @ layer
+
+        return result
 
     @staticmethod
     def loss(model, x: Array, y: Array) -> Array:
@@ -49,6 +63,14 @@ class DeepLinearNetwork:
     @property
     def num_parameters(self) -> int:
         return sum(jnp.size(layer) for layer in self.layers)
+    
+    @property
+    def dim_in(self) -> int:
+        return self.layers[0].shape[0]
+    
+    @property
+    def dim_out(self) -> int:
+        return self.layers[-1].shape[1]
 
     @classmethod
     def initialize(cls, key: Key, layer_sizes: List[int]):
@@ -58,8 +80,11 @@ class DeepLinearNetwork:
         shapes = zip(layer_sizes, layer_sizes[1:])
         key_init_layers = jax.random.split(key, num=len(layer_sizes) - 1)
 
-        # TODO check this
-        layers = [jax.random.normal(k, shape) for k, shape in zip(key_init_layers, shapes)]
+        # layers = [jax.random.normal(k, shape) for k, shape in zip(key_init_layers, shapes)]
+        # layers = [jax.random.truncated_normal(k, shape=shape, upper=1, lower=-1) for k, shape in zip(key_init_layers, shapes)]
+        # layers = [jax.random.uniform(k, shape=shape, minval=-0.5, maxval=0.5) for k, shape in zip(key_init_layers, shapes)]
+        layers = [xavier_normal_init(k, shape) for k, shape in zip(key_init_layers, shapes)]
+        # layers = [xavier_uniform_init(k, shape) for k, shape in zip(key_init_layers, shapes)]
 
         return cls(layers=layers)
     

projects/shared/samplers_optax.py
@@ -4,64 +4,40 @@ from jaxtyping import PRNGKeyArray as Key, Array
 from typing import Tuple, Any
 
 
-from optax import GradientTransformation
-
-
-
 SGLDState = Tuple[Key, Any]
 
 
 
 class SGLD:
 
-    def __init__(self, epsilon, gamma, nbeta, seed):
-        self.seed = seed
+    def __init__(self, epsilon, gamma, nbeta):
         self.epsilon = epsilon
         self.gamma = gamma
         self.nbeta = nbeta
 
-        self.sqrt_epsilon = jnp.sqrt(self.epsilon)
-        self.epsilon_nbeta = epsilon * nbeta / 2
-        self.epsilon_gamma = epsilon * gamma / 2
-
-    def init(self, params) -> SGLDState:
-        return jax.random.key(self.seed), params
+    def init(self, params, key: Key) -> SGLDState:
+        return key, params
 
     def update(self, grads, state: SGLDState, params) -> Tuple[Any, SGLDState]:
         
         def single_param_update(key: Key, param: Array, init_param: Array, grad: Array) -> Array:
-            print(f"param: {param.shape}")
-            noise = self.sqrt_epsilon * jax.random.normal(key, shape=param.shape)
-            prior_force = self.epsilon_gamma * (param - init_param)
-            grad_force = self.epsilon_nbeta * grad
-
             
-
-            print(f"\tNoise: min={noise.min()}, max={noise.max()}")
-            print(f"\tPrior: min={prior_force.min()}, max={prior_force.max()}")
-            print(f"\tGrad: min={grad_force.min()}, max={grad_force.max()}")
-
+            noise = jnp.sqrt(self.epsilon) * jax.random.normal(key, shape=param.shape)
             
-
-            return -prior_force - grad_force + noise
+            return -0.5 * self.epsilon * self.gamma * (param - init_param) - 0.5 * self.epsilon * self.nbeta * grad + noise
         
         key, init_params = state
+        key_next, key_curr = jax.random.split(key)
+
 
         params_treedef = jax.tree.structure(params)
-        keys = jax.random.split(key, num=params_treedef.num_leaves + 1)
+        keys_param_updates = jax.random.split(key_curr, num=params_treedef.num_leaves)
         
         
-        keys_updates = jax.tree.unflatten(params_treedef, keys[1:])
-        key_state = keys[0]
-
+        keys_updates = jax.tree.unflatten(params_treedef, keys_param_updates)
         updates = jax.tree_map(single_param_update, keys_updates, params, init_params, grads)
-        
 
-        updates_flat, _ = jax.tree.flatten(updates)
-        
-
-
-        return updates, (key_state, init_params)
+        return updates, (key_next, init_params)
 
     
     